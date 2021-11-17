9cee31871b326e8a13a38b23acabe097510efff0
Nastassia Kazak <nastya.kazak.97@yandex.by>
2021-11-17 18:07:56 +0300

    feat: add popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,31 @@
-console.log('loaded')
-
 const aboutLink = document.querySelector(".profile__edit-button");
 const popup = document.querySelector(".popup");
 const popupCloseButton = popup.querySelector(".popup__close");
-const popupOverlay = popup.querySelector(".popup__container");
-const title = document.querySelector(".profile__name");
+const formElement = popup.querySelector(".popup__form");
+const nameInput = popup.querySelector(".popup__name");
+const jobInput = popup.querySelector(".popup__bio");
+const submit = popup.querySelector('.popup__save-button');
+const profileName = document.querySelector('.profile__name');
+const profileBio = document.querySelector('.profile__bio');
+
 
 function open() {
-    popup.classList.add("popup_opened");
+  popup.classList.add("popup_opened");
 }
 
 function close() {
-    title.textContent = "NEW CONTENT";
-    popup.classList.remove("popup_opened");
+  popup.classList.remove("popup_opened")
 }
 
-if (aboutLink) {
-    aboutLink.addEventListener('click', open);
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+
+  profileName.textContent = nameInput.value;
+  profileBio.textContent = jobInput.value;
+
+  close ();
 }
 
+aboutLink.addEventListener('click', open);
 popupCloseButton.addEventListener('click', close);
-popupOverlay.addEventListener('click', close);
+formElement.addEventListener('submit', formSubmitHandler);
