acfe3db246cb60c56cd57864e2645d3911b0cdc6
Nastassia Kazak <nastya.kazak.97@yandex.by>
2021-11-28 20:49:18 +0300

    feat: dynamic cards

--- scripts/index.js
+++ scripts/index.js
@@ -32,3 +32,68 @@ function formSubmitHandler (evt) {
 aboutLink.addEventListener('click', open);
 popupCloseButton.addEventListener('click', close);
 formElement.addEventListener('submit', formSubmitHandler);
+
+/* Проектная работа 5 */
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+    alt: 'Горы Архыза с остатками талого снега'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
+    alt: 'Зимний пейзаж реки в Челябинской области'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
+    alt: 'Ряд многоэтажных домов в Иваново'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
+    alt: 'Флора у подножия гор на Камчатке'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
+    alt: 'Железная дорога в лесу Холмогорского района'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
+    alt: 'Снежные горы Байкала'
+  }
+];
+
+const listContainerEl = document.querySelector('.photo-grid');
+const templateEl = document.querySelector('.template');
+
+function getItem(item) {
+  const newItem = templateEl.content.cloneNode(true);
+
+  const imageEl = newItem.querySelector('.card__image');
+  imageEl.src = item.link;
+
+  const imageAltEl = newItem.querySelector('.card__image');
+  imageAltEl.alt = item.alt;
+
+  const headerEl = newItem.querySelector('.card__title');
+  headerEl.textContent = item.name;
+
+  return newItem;
+}
+
+function render() {
+  const html = initialCards.map((item) => {
+    return getItem(item);
+  });
+
+  listContainerEl.append(...html);
+}
+
+render();
+
+
